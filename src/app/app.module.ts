ab4182bff5615d1f32260e01d50f2fdf411177a9
Amarjeet Soni <amarjeetsoni27@gmail.com>
2021-09-28 14:33:50 +0530

    some new files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeModuleComponent } from './home-module/home-module.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeModuleComponent
   ],
   imports: [
     BrowserModule
